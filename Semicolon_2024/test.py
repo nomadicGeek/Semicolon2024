7665533a6cd5927d22c20f43b025741ca0be8171
Chintamani <chintamani.virulkar@gmail.com>
2024-02-23 12:50:03 +0530

    added fetch email code in SEMICOLO24.test.py file

--- Semicolon_2024/test.py
+++ Semicolon_2024/test.py
@@ -1,17 +1,20 @@
-import pandas as pd
-from keras.models import load_model
+import openai
+from django.core.mail import send_mail
 
-df = pd.read_csv('EEG\static\emotions.csv')
-df = df.drop('label', axis=1)
-df = df.drop(index=df.index[0], axis=0)
-model = load_model('EEG\static\model.h5')
-predicted_emotion = model.predict(df)
-emotions = []
-for i in predicted_emotion:
-    if i[0] == 1.0:
-        emotions.append("Positive")
-    elif i[1] == 1.0:
-        emotions.append("Negative")
-    elif i[2] == 1.0:
-        emotions.append("Neutral")
-print(emotions[0])
+openai.api_key = "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"
+age = '35'
+gender = 'Male'
+employeeName = 'Chintamani'
+prompt = "draft me personalised email to send as a report of mental wellness of employee with name whos is receieint "+employeeName+" in json format with keys Subject and body with recepient name, email body will have that employee's mental health is not good to work and sugestion to deal with mental illness"
+print(prompt)
+response = openai.ChatCompletion.create(
+    model='gpt-3.5-turbo',
+    messages=[
+        {"role": "system", "content": prompt}
+    ])
+
+output = response.choices[0]['message']['content']
+dict = eval(output)
+print(output)
+print(dict)
+
