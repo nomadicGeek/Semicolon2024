4eb2a4d10d55fc372b2353cce93dde220e517182
Rohit <rohitsav1305@gmail.com>
2024-02-24 10:00:55 +0530

    urls py conflict sorted

--- EEG/urls.py
+++ EEG/urls.py
@@ -6,13 +6,10 @@ from .views import upload_file
 
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("", views.index, name="index"),
-    path('input/', views.input_form, name='input_form'),
-    path('process_input/', views.process_input, name='process_input'),
-=======
+    #path("", views.index, name="index"),
+    #path('input/', views.input_form, name='input_form'),
+    #path('process_input/', views.process_input, name='process_input'),
     path("", views.upload_file, name="upload_file"),
->>>>>>> 65c18e7ffb15100ea961805a78c4819875683290
     path('eeg/', views.eeg_view, name='eeg_view'),
     path('upload/', views.upload_file, name='upload_file'),
     path('suggestion/', views.suggestionsChatGPT, name='suggestionsChatGPT'),
